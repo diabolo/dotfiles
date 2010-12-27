0886538a54b20128a8f651c1cf79d46187dc068c
Andrew Premdas <apremdas@yahoo.co.uk>
2010-12-27 09:16:49 +0000

    modified git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,9 +17,14 @@ alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gch='git checkout'
 alias gb='git branch'
+alias gba='git branch -a'
+alias ga='git add'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+# pretty sure this doesn't work
+# alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+function gitrm() { git status | grep deleted | awk '{print$3}' | xargs git rm;}
