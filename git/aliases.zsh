abfe324b1a7f223424f96496e7a59f3908b79e71
Andrew Premdas <apremdas@yahoo.co.uk>
2010-12-31 07:30:32 +0000

    added gst alias, modified hub alias to use default rvm ruby

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,11 +5,13 @@
 # regardless of which Ruby you're using or else the `git status` in your prompt
 # will take seven thousand seconds to run `ls`.
 #
-# I'm hardcoding it to an installed rvm (using rvm's `rvm 1.8.7,ruby /hub/path`
-# syntax is way too slow). It should work fine for those without rvm, though.
+# I'm hardcoding it to rvm's default ruby. If this isn't set then things will go 
+# bang. Using rvm's `rvm 1.8.7,ruby /hub/path` syntax is way too slow). 
+# This won't effect non rvm users.
 if [[ -s $HOME/.rvm/scripts/rvm ]] 
 then
-  alias git='$HOME/.rvm/rubies/ruby-1.8.7-p302/bin/ruby `which hub`'
+  # alias git='$HOME/.rvm/rubies/ruby-1.8.7-p302/bin/ruby `which hub`'
+  alias git='$HOME/.rvm/rubies/default/bin/ruby `which hub`'
 fi
 
 # The rest of my fun git aliases
@@ -24,6 +26,7 @@ alias gb='git branch'
 alias gba='git branch -a'
 alias ga='git add'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gst='git status'
 # pretty sure this doesn't work
 # alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 
